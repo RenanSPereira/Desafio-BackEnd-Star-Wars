1de79ba519224c3ef25fefb4b045888f23d994eb
Renan Santos <renan3santos@gmai.com>
2021-01-24 20:40:45 -0300

    Criando Model Planets

--- planets/models.py
+++ planets/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Planet(models.Model):
+    name = models.TextField(max_length=50, null=False, blank=False)
+    climate = models.TextField(max_length=20, null=False, blank=False)
+    terrain = models.TextField(max_length=20, null=False, blank=False)
+
+    def __str__(self):
+        return self.name

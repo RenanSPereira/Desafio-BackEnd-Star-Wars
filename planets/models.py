7cf711bc4073fa8f04ef54355c59c86eb1388f68
Renan Santos <renan3santos@gmai.com>
2021-01-24 21:00:21 -0300

    Adicionando Novos campos para o modelo

--- planets/models.py
+++ planets/models.py
@@ -5,6 +5,9 @@ class Planet(models.Model):
     name = models.TextField(max_length=50, null=False, blank=False)
     climate = models.TextField(max_length=20, null=False, blank=False)
     terrain = models.TextField(max_length=20, null=False, blank=False)
+    created = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)    
+    films_qty = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.name

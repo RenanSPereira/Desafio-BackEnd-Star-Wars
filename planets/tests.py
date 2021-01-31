8eb332cbaecbbe569032546898b2567872da179f
Renan Santos <renan3santos@gmai.com>
2021-01-31 18:10:21 -0300

    Criando Testes para app Planets

--- planets/tests.py
+++ planets/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .swapi_service import get_films_qty
+from .models import Planet
 
-# Create your tests here.
+
+class SWAPTests(APITestCase):
+
+    def test_get_films_qty(self):
+        response = get_films_qty('tatooine')
+        self.assertEquals(response, 5)
+    
+    def test_get_films_qty_not_exist_planet(self):
+        response = get_films_qty('null')
+        self.assertEquals(response, 0)
+
+
+class PlanetTests(APITestCase):
+
+    def setUp(self):
+        self.url = 'http://127.0.0.1:8000/planets/'
+        self.data = {
+            'name': 'tatooine', 
+            'climate': 'desert', 
+            'terrain': 'Arid', 
+            'films_qty': get_films_qty('tatooine')
+        }
+    
+    def test_create_planet(self):
+        response = self.client.post(self.url, self.data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Planet.objects.get().name, 'tatooine')
+        self.assertEqual(Planet.objects.get().films_qty, 5)
+
+    def test_planet_list(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
